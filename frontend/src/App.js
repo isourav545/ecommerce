5a42fab39cf4fbeb8b1a84e374429898d293d63c
Sourav Ghosh <souravghosh95460@gmail.com>
2022-06-04 18:24:45 +0530

    frontend changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
-const App= ()=> {
+const App = () => {
   return (
     <>
-    <h1>Welcome to Proshop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+         <HomeScreen/>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
